7c05ff404838beb6f001c025fe2652f6f46d2a73
VenkateshSrini <venkateshsrini3@gmail.com>
2022-07-28 09:39:31 +0530

    added code in for entity object persistence

---
+++ dapr.state.console/Model/EntityObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dapr.state.console.Model
+{
+    public class EntityObject
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- dapr.state.console/Worker.cs
+++ dapr.state.console/Worker.cs
@@ -1,3 +1,4 @@
+using dapr.state.console.Model;
 using Dapr.Client;
 
 namespace dapr.state.console
@@ -6,18 +7,31 @@ namespace dapr.state.console
     {
         private readonly ILogger<Worker> _logger;
         private readonly DaprClient _daprClient;
-        public Worker(ILogger<Worker> logger, DaprClient daprClient )
+        private readonly IConfiguration _configuration;
+        public Worker(ILogger<Worker> logger, DaprClient daprClient,
+            IConfiguration configuration)
         {
             _logger = logger;
             _daprClient = daprClient;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+                var stateStore = _configuration["State.Store"];
+                
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var cacheObj = new EntityObject
+                {
+                    Id = System.Guid.NewGuid().ToString(),
+                    Name = $"Obj-{DateTime.Now}"
+                };
+                await _daprClient.SaveStateAsync<EntityObject>(stateStore, cacheObj.Id,
+                    cacheObj);
+                var newObj = await _daprClient.GetStateAsync<EntityObject>(stateStore, cacheObj.Id);
+                _logger.LogInformation($"Worker {newObj.Name}");
             }
         }
         public override Task StopAsync(CancellationToken cancellationToken)
